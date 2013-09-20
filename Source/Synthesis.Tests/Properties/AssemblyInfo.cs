2944057679eb7ba5bb447ba1d3534bccdef22038
Kam Figy <kamsar@gmail.com>
2013-09-19 19:46:29 -0700

    Fix up assembly versioning and nuspecs

--- Source/Synthesis.Tests/Properties/AssemblyInfo.cs
+++ Source/Synthesis.Tests/Properties/AssemblyInfo.cs
@@ -1,35 +1,13 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Synthesis.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Synthesis unit tests")]
 [assembly: AssemblyCompany("ISITE Design")]
-[assembly: AssemblyProduct("Synthesis.Tests")]
-[assembly: AssemblyCopyright("Copyright 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyProduct("Synthesis")]
+[assembly: AssemblyCopyright("Copyright © Kam Figy, ISITE Design")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0729e39c-8963-4cb8-9e77-b05915b5da3a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.0.2.0")]
-[assembly: AssemblyFileVersion("5.0.2.0")]
+[assembly: AssemblyVersion("7.0.0.0")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
+[assembly: CLSCompliant(false)]
--- Source/Synthesis/Properties/AssemblyInfo.cs
+++ Source/Synthesis/Properties/AssemblyInfo.cs
@@ -3,39 +3,13 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Synthesis")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Sitecore object mapping framework")]
 [assembly: AssemblyCompany("ISITE Design")]
 [assembly: AssemblyProduct("Synthesis")]
 [assembly: AssemblyCopyright("Copyright © Kam Figy, ISITE Design")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fdf50f81-702b-4eca-8e54-d8f9196787c8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.0.0.0")]
-
+[assembly: AssemblyVersion("7.0.0.0")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
 [assembly: InternalsVisibleTo("Synthesis.Tests")]
-
 [assembly: CLSCompliant(false)]
