a4614eed8bdc04dcb0a155695d9ecb2b9ef4060c
Kam Figy <kamsar@gmail.com>
2014-04-29 09:32:56 -0700

    Fix build configuration

--- Build/PackageNuGet.ps1
+++ Build/PackageNuGet.ps1
@@ -12,12 +12,12 @@ $targetAssemblyVersion = $synthesisAssembly.ProductVersion
 
 & $nuGet pack "$scriptRoot\Synthesis.nuget\Synthesis.nuspec" -version $targetAssemblyVersion
 
-& $nuGet pack "$scriptRoot\..\Source\Synthesis\Synthesis.csproj" -Symbols
+& $nuGet pack "$scriptRoot\..\Source\Synthesis\Synthesis.csproj" -Symbols -Prop Configuration=Release
 
-& $nuGet pack "$scriptRoot\..\Source\Synthesis.Testing\Synthesis.Testing.csproj" -Symbols
+& $nuGet pack "$scriptRoot\..\Source\Synthesis.Testing\Synthesis.Testing.csproj" -Symbols -Prop Configuration=Release
 
 & $nuGet pack "$scriptRoot\Synthesis.Blade.nuget\Synthesis.Blade.nuspec" -version $targetAssemblyVersion
 
-& $nuGet pack "$scriptRoot\..\Source\Synthesis.Blade\Synthesis.Blade.csproj" -Symbols
+& $nuGet pack "$scriptRoot\..\Source\Synthesis.Blade\Synthesis.Blade.csproj" -Symbols -Prop Configuration=Release
 
-& $nuGet pack "$scriptRoot\..\Source\Synthesis.Blade.Ninject\Synthesis.Blade.Ninject.csproj" -Symbols
+& $nuGet pack "$scriptRoot\..\Source\Synthesis.Blade.Ninject\Synthesis.Blade.Ninject.csproj" -Symbols -Prop Configuration=Release
