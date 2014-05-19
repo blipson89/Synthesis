830145bcf9da939551f7547c1c11884f7f9dea54
Martijn Bos <martijn.bos@redhotminute.com>
2014-05-19 16:25:12 +0200

    Return null in case of no items instead an error.
    Return a null in
    Synthesis.FieldTypes.Adapters.AxesAdapter.SelectItems and
    Synthesis.FieldTypes.Adapters.AxesAdapter.SelectSingleItem

--- Source/Synthesis/FieldTypes/Adapters/AxesAdapter.cs
+++ Source/Synthesis/FieldTypes/Adapters/AxesAdapter.cs
@@ -6,92 +6,94 @@ using Sitecore.Data.Items;
 
 namespace Synthesis.FieldTypes.Adapters
 {
-	/// <summary>
-	///     Version of ItemAxes that returns strongly typed entities. It also moves the Item.GetChildren() and Item.Parent in here, since they are axes really.
-	/// </summary>
-	public class AxesAdapter : IAxesAdapter
-	{
-		private readonly ItemAxes _axes;
-		private readonly Item _item;
+    /// <summary>
+    ///     Version of ItemAxes that returns strongly typed entities. It also moves the Item.GetChildren() and Item.Parent in here, since they are axes really.
+    /// </summary>
+    public class AxesAdapter : IAxesAdapter
+    {
+        private readonly ItemAxes _axes;
+        private readonly Item _item;
 
-		public AxesAdapter(Item item)
-		{
-			_item = item;
-			_axes = new ItemAxes(item);
-		}
+        public AxesAdapter(Item item)
+        {
+            _item = item;
+            _axes = new ItemAxes(item);
+        }
 
-		public IStandardTemplateItem Parent
-		{
-			get { return _item.Parent.AsStronglyTyped(); }
-		}
+        public IStandardTemplateItem Parent
+        {
+            get { return _item.Parent.AsStronglyTyped(); }
+        }
 
-		public IStandardTemplateItem[] GetAncestors()
-		{
-			return _axes.GetAncestors().AsStronglyTypedCollection().ToArray();
-		}
+        public IStandardTemplateItem[] GetAncestors()
+        {
+            return _axes.GetAncestors().AsStronglyTypedCollection().ToArray();
+        }
 
-		public IStandardTemplateItem GetChild(ID childId)
-		{
-			return _axes.GetChild(childId).AsStronglyTyped();
-		}
+        public IStandardTemplateItem GetChild(ID childId)
+        {
+            return _axes.GetChild(childId).AsStronglyTyped();
+        }
 
-		public IStandardTemplateItem GetChild(string itemName)
-		{
-			return _axes.GetChild(itemName).AsStronglyTyped();
-		}
+        public IStandardTemplateItem GetChild(string itemName)
+        {
+            return _axes.GetChild(itemName).AsStronglyTyped();
+        }
 
-		public IStandardTemplateItem GetDescendant(string name)
-		{
-			return _axes.GetDescendant(name).AsStronglyTyped();
-		}
+        public IStandardTemplateItem GetDescendant(string name)
+        {
+            return _axes.GetDescendant(name).AsStronglyTyped();
+        }
 
-		[SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Following Sitecore Axes naming")]
-		public IEnumerable<IStandardTemplateItem> GetDescendants()
-		{
-			return _axes.GetDescendants().AsStronglyTypedCollection();
-		}
+        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Following Sitecore Axes naming")]
+        public IEnumerable<IStandardTemplateItem> GetDescendants()
+        {
+            return _axes.GetDescendants().AsStronglyTypedCollection();
+        }
 
-		public IStandardTemplateItem GetItem(string path)
-		{
-			return _axes.GetItem(path).AsStronglyTyped();
-		}
+        public IStandardTemplateItem GetItem(string path)
+        {
+            return _axes.GetItem(path).AsStronglyTyped();
+        }
 
-		[SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Following Sitecore Axes naming")]
-		public IStandardTemplateItem GetNextSibling()
-		{
-			return _axes.GetNextSibling().AsStronglyTyped();
-		}
+        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Following Sitecore Axes naming")]
+        public IStandardTemplateItem GetNextSibling()
+        {
+            return _axes.GetNextSibling().AsStronglyTyped();
+        }
 
-		[SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Following Sitecore Axes naming")]
-		public IStandardTemplateItem GetPreviousSibling()
-		{
-			return _axes.GetNextSibling().AsStronglyTyped();
-		}
+        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Following Sitecore Axes naming")]
+        public IStandardTemplateItem GetPreviousSibling()
+        {
+            return _axes.GetNextSibling().AsStronglyTyped();
+        }
 
-		public bool IsAncestorOf(IStandardTemplateItem item)
-		{
-			return _axes.IsAncestorOf(item.InnerItem);
-		}
+        public bool IsAncestorOf(IStandardTemplateItem item)
+        {
+            return _axes.IsAncestorOf(item.InnerItem);
+        }
 
-		public bool IsDescendantOf(IStandardTemplateItem item)
-		{
-			return _axes.IsDescendantOf(item.InnerItem);
-		}
+        public bool IsDescendantOf(IStandardTemplateItem item)
+        {
+            return _axes.IsDescendantOf(item.InnerItem);
+        }
 
-		public IEnumerable<IStandardTemplateItem> SelectItems(string query)
-		{
-			return _axes.SelectItems(query).AsStronglyTypedCollection();
-		}
+        public IEnumerable<IStandardTemplateItem> SelectItems(string query)
+        {
+            Item[] selectItems = _axes.SelectItems(query);
+            return selectItems == null ? null : selectItems.AsStronglyTypedCollection();
+        }
 
-		public IStandardTemplateItem SelectSingleItem(string query)
-		{
-			return _axes.SelectSingleItem(query).AsStronglyTyped();
-		}
+        public IStandardTemplateItem SelectSingleItem(string query)
+        {
+            Item selectSingleItem = _axes.SelectSingleItem(query);
+            return selectSingleItem == null ? null : selectSingleItem.AsStronglyTyped();
+        }
 
-		[SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Following Sitecore Axes naming")]
-		public IEnumerable<IStandardTemplateItem> GetChildren()
-		{
-			return _item.GetChildren().AsStronglyTypedCollection();
-		}
-	}
+        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Following Sitecore Axes naming")]
+        public IEnumerable<IStandardTemplateItem> GetChildren()
+        {
+            return _item.GetChildren().AsStronglyTypedCollection();
+        }
+    }
 }
