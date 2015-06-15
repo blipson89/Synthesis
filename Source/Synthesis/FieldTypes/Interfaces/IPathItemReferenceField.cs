edfc63ecf114f7e854976bfbea5d5340a272ab91
jeffdarchuk <jdarchuk@connectivedx.com>
2015-06-15 11:15:30 -0700

    Adding internal field support to Synthesis
    all that needed to be done was add an interface and a field for getting a
    refernce item via path instead of GUID and modify the config file.

---
+++ Source/Synthesis/FieldTypes/Interfaces/IPathItemReferenceField.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Data.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synthesis.FieldTypes.Interfaces
+{
+	public interface IPathItemReferenceField : IFieldType
+	{
+		/// Gets the item ID that the relationship refers to
+		/// </summary>
+		string TargetPath { get; set; }
+
+		/// <summary>
+		/// Gets the entity that the relationship is to. Returns null if the entity doesn't exist.
+		/// </summary>
+		IStandardTemplateItem Target { get; }
+
+		ReferenceField ToReferenceField();
+	}
+}
