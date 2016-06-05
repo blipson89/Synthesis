a2920fc7b9aba59a9858e4ccf1ec7794d9ec07ca
Kam Figy <kamsar@gmail.com>
2016-06-04 21:51:01 -0700

    Synthesis configuration autoregistration setup
    Configurations may now register themselves using code, similar to MVC area registrations.
    Config registration is made much simpler with the addition of the SynthesisConfigurationRegistration class, which simplifies dependency management
    The SynthesisConfigRegistrar pipeline processor may be used to autoregister configurations in one or more assemblies like the type list provider (including wildcard assembly names)
    
    The default synthesis configuration registers itself by scanning the Synthesis assembly itself and autoregistering itself.

---
+++ Source/Synthesis/Configuration/Registration/ProxyGeneratorParametersProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Synthesis.Generation;
+
+namespace Synthesis.Configuration.Registration
+{
+	/// <summary>
+	/// Wraps an inner generator parameters provider and allows mutating its resultant parameters
+	/// </summary>
+	public class ProxyGeneratorParametersProvider : IGeneratorParametersProvider
+	{
+		private readonly IGeneratorParametersProvider _innerProvider;
+		private readonly Action<GeneratorParameters> _permutationAction;
+
+		public ProxyGeneratorParametersProvider(IGeneratorParametersProvider innerProvider, Action<GeneratorParameters> permutationAction)
+		{
+			_innerProvider = innerProvider;
+			_permutationAction = permutationAction;
+		}
+
+
+		public GeneratorParameters CreateParameters(string configurationName)
+		{
+			// we call base here so the parameters begin as a copy of what's in global config (e.g. base classes, etc)
+			var parameters = _innerProvider.CreateParameters(configurationName);
+
+			_permutationAction(parameters);
+
+			return parameters;
+		}
+	}
+}
---
+++ Source/Synthesis/Pipelines/Initialize/SynthesisConfigRegistrar.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines;
+using Synthesis.Configuration;
+using Synthesis.Configuration.Registration;
+using Synthesis.Utility;
+
+namespace Synthesis.Pipelines.Initialize
+{
+	/// <summary>
+	/// Registers all Synthesis configurations defined in a specific set of assemblies.
+	/// To register the Synthesis default configuration, load configurations in the Synthesis assembly.
+	/// </summary>
+	public class SynthesisConfigRegistrar
+	{
+		private readonly List<Assembly> _assemblies = new List<Assembly>();
+
+		public virtual void Process(PipelineArgs args)
+		{
+			var types = GetTypesInRegisteredAssemblies();
+			var configurations = GetConfigurationsFromTypes(types);
+
+			foreach (var configRegistration in configurations)
+			{
+				ProviderResolver.RegisterConfiguration(configRegistration.GetConfiguration());
+			}
+		}
+
+		[SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "We want to ignore all load errors on assembly types")]
+		protected virtual IEnumerable<Type> GetTypesInRegisteredAssemblies()
+		{
+			if (_assemblies.Count == 0) throw new InvalidOperationException("You must specify the assemblies to scan for Synthesis configurations, e.g. <assemblies hint=\"list: AddAssembly\"><default>Synthesis</default></assemblies>");
+
+			IEnumerable<Assembly> assemblies = _assemblies;
+
+			return assemblies.SelectMany(delegate (Assembly x)
+			{
+				try { return x.GetExportedTypes(); }
+				catch (ReflectionTypeLoadException rex) { return rex.Types.Where(y => y != null).ToArray(); } // http://haacked.com/archive/2012/07/23/get-all-types-in-an-assembly.aspx
+							catch { return new Type[] { }; }
+			}).ToList();
+		}
+
+		protected virtual IEnumerable<ISynthesisConfigurationRegistration> GetConfigurationsFromTypes(IEnumerable<Type> types)
+		{
+			return types
+				.Where(type => typeof (ISynthesisConfigurationRegistration).IsAssignableFrom(type) && !type.IsAbstract && !type.IsInterface)
+				.Select(Activator.CreateInstance)
+				.OfType<ISynthesisConfigurationRegistration>()
+				.ToArray();
+		}
+
+		public void AddAssembly(string name)
+		{
+			if (name.Contains("*"))
+			{
+				var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+				foreach (var assembly in assemblies)
+				{
+					if (WildcardUtility.IsWildcardMatch(assembly.FullName, name)) AddAssembly(assembly.FullName);
+				}
+
+				return;
+			}
+
+			Assembly a = Assembly.Load(name);
+			if (a == null) throw new ArgumentException("The assembly name was not valid");
+
+			_assemblies.Add(a);
+		}
+	}
+}
