e8ea0391487de26c6b7ff3a9cc445e9bc5cc793e
Kam Figy <kamsar@gmail.com>
2016-10-20 06:52:20 -0700

    8.2.2 RC1

--- Source/SharedAssemblyInfo.cs
+++ Source/SharedAssemblyInfo.cs
@@ -6,6 +6,6 @@ using System;
 [assembly: AssemblyProduct("Synthesis")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("8.2.1.0")]
-[assembly: AssemblyFileVersion("8.2.1.0")]
-[assembly: AssemblyInformationalVersion("8.2.1")]
+[assembly: AssemblyFileVersion("8.2.2.0")]
+[assembly: AssemblyInformationalVersion("8.2.2-RC01")]
 [assembly: CLSCompliant(false)]
