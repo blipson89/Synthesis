c36bb165e4528360fc8cdd9d69ee9a6fe07c7405
Ben Lipson <blipson89@gmail.com>
2017-12-27 14:45:11 -0500

    Updated versioning

--- Source/SharedAssemblyInfo.cs
+++ Source/SharedAssemblyInfo.cs
@@ -1,11 +1,11 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System;
 
 [assembly: AssemblyCompany("Connective DX")]
 [assembly: AssemblyProduct("Synthesis")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("8.2.6.0")]
-[assembly: AssemblyFileVersion("8.2.6.0")]
-[assembly: AssemblyInformationalVersion("8.2.6")]
+[assembly: AssemblyVersion("8.2.7.0")]
+[assembly: AssemblyFileVersion("8.2.7.0")]
+[assembly: AssemblyInformationalVersion("8.2.7-RC01")]
 [assembly: CLSCompliant(false)]
