6b15ecabf30d5a03fef9e955c7e837baf27969fe
Ben Lipson <blipson89@gmail.com>
2018-04-12 09:58:52 -0400

    Fixed improper rendering of Multi-Line Text fields when isEditable is false

---
+++ Source/Synthesis.Mvc/Utility/DisplayModeSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Sitecore.Sites;
+
+namespace Synthesis.Mvc.Utility
+{
+	public class DisplayModeSwitcher : IDisposable
+	{
+		private readonly DisplayMode _originalMode;
+
+		public DisplayModeSwitcher(DisplayMode newDisplayMode)
+		{
+			_originalMode = Sitecore.Context.Site.DisplayMode;
+			Sitecore.Context.Site.SetDisplayMode(newDisplayMode, DisplayModeDuration.Temporary);
+		}
+
+		public void Dispose()
+		{
+			Sitecore.Context.Site.SetDisplayMode(_originalMode, DisplayModeDuration.Temporary);
+		}
+	}
+}
