ba8f4351b742f515d550c1307d516c0377d1d478
Ben Lipson <blipson89@gmail.com>
2018-12-05 19:11:40 -0500

    9.1.0.1-beta1

--- Source/SharedAssemblyInfo.cs
+++ Source/SharedAssemblyInfo.cs
@@ -2,10 +2,10 @@
 using System.Runtime.InteropServices;
 using System;
 
-[assembly: AssemblyCompany("Connective DX")]
+[assembly: AssemblyCompany("Synthesis")]
 [assembly: AssemblyProduct("Synthesis")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("9.0.1")]
-[assembly: AssemblyFileVersion("9.0.1")]
-[assembly: AssemblyInformationalVersion("9.0.1")]
+[assembly: AssemblyVersion("9.1.0")]
+[assembly: AssemblyFileVersion("9.1.0")]
+[assembly: AssemblyInformationalVersion("9.1.0.1-beta1")]
 [assembly: CLSCompliant(false)]
