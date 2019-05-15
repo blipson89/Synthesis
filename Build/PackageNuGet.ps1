b59cb5ebf46732843fc6ed2718f2f0cdefc7b18c
Ben Lipson <blipson89@gmail.com>
2019-05-15 17:58:25 -0400

    Update to legacy nuget packager script

--- Build/PackageNuGet.ps1
+++ Build/PackageNuGet.ps1
@@ -29,19 +29,20 @@ $solution = "$scriptRoot\..\Synthesis.sln"
 & $nuGet restore $solution
 & $msBuild $solution /p:Configuration=Release /t:Rebuild /m
 
-$synthesisAssembly = Get-Item "$scriptRoot\..\Source\Synthesis\bin\Release\Synthesis.dll" | Select-Object -ExpandProperty VersionInfo
+$synthesisAssembly = Get-Item "$scriptRoot\..\Source\Synthesis\bin\Release\*\Synthesis.dll" | Select-Object -ExpandProperty VersionInfo
+
 $targetAssemblyVersion = $synthesisAssembly.ProductVersion
 
-& $nuGet pack "$scriptRoot\Synthesis.nuget\Synthesis.nuspec" -version $targetAssemblyVersion
+& $nuGet pack "$scriptRoot\Synthesis.nuget\Synthesis.nuspec" -version $targetAssemblyVersion -OutputDirectory "$scriptRoot\Versions\$targetAssemblyVersion"
 
-& $nuGet pack "$scriptRoot\..\Source\Synthesis\Synthesis.csproj" -Symbols -Prop Configuration=Release
+& $nuGet pack "$scriptRoot\..\Source\Synthesis\Synthesis.csproj" -Symbols -Prop Configuration=Release -OutputDirectory "$scriptRoot\Versions\$targetAssemblyVersion"
 
-& $nuGet pack "$scriptRoot\..\Source\Synthesis.Testing\Synthesis.Testing.csproj" -Symbols -Prop Configuration=Release
+& $nuGet pack "$scriptRoot\..\Source\Synthesis.Testing\Synthesis.Testing.csproj" -Symbols -Prop Configuration=Release -OutputDirectory "$scriptRoot\Versions\$targetAssemblyVersion"
 
-& $nuGet pack "$scriptRoot\Synthesis.Mvc.nuget\Synthesis.Mvc.nuspec" -version $targetAssemblyVersion
+& $nuGet pack "$scriptRoot\Synthesis.Mvc.nuget\Synthesis.Mvc.nuspec" -version $targetAssemblyVersion -OutputDirectory "$scriptRoot\Versions\$targetAssemblyVersion"
 
-& $nuGet pack "$scriptRoot\..\Source\Synthesis.Mvc\Synthesis.Mvc.csproj" -Symbols -Prop Configuration=Release
+& $nuGet pack "$scriptRoot\..\Source\Synthesis.Mvc\Synthesis.Mvc.csproj" -Symbols -Prop Configuration=Release -OutputDirectory "$scriptRoot\Versions\$targetAssemblyVersion"
 
-& $nuGet pack "$scriptRoot\Synthesis.Solr.nuget\Synthesis.Solr.nuspec" -version $targetAssemblyVersion
+& $nuGet pack "$scriptRoot\Synthesis.Solr.nuget\Synthesis.Solr.nuspec" -version $targetAssemblyVersion -OutputDirectory "$scriptRoot\Versions\$targetAssemblyVersion"
 
-& $nuGet pack "$scriptRoot\..\Source\Synthesis.Solr\Synthesis.Solr.csproj" -Symbols -Prop Configuration=Release
+& $nuGet pack "$scriptRoot\..\Source\Synthesis.Solr\Synthesis.Solr.csproj" -Symbols -Prop Configuration=Release -OutputDirectory "$scriptRoot\Versions\$targetAssemblyVersion"
