c0055ec13c3ffd3c3472a4ffd662c3ad08d4f64c
Ben Lipson <blipson89@gmail.com>
2021-06-05 14:30:05 -0400

    #93 - Created ContentHubImageField for CH images

---
+++ Source/Synthesis/FieldTypes/Interfaces/IContentHubImageField.cs
@@ -0,0 +1,20 @@
+﻿namespace Synthesis.FieldTypes.Interfaces
+{
+	public interface IContentHubImageField : IImageField
+	{
+		/// <summary>
+		/// Gets the StyleLabs Content ID
+		/// </summary>
+		string ContentId { get; }
+
+		/// <summary>
+		/// Gets the Thumbnail Src
+		/// </summary>
+		string ThumbnailSrc { get; }
+
+		/// <summary>
+		/// Gets the StyleLabs Content Type
+		/// </summary>
+		string ContentType { get; }
+	}
+}
---
+++ Source/Synthesis/FieldTypes/XmlFieldType.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Data.Fields;
+
+namespace Synthesis.FieldTypes
+{
+	public abstract class XmlFieldType : FieldType
+	{
+		protected XmlFieldType(LazyField innerField, string searchValue) : base(innerField, searchValue)
+		{
+		}
+
+		/// <summary>
+		/// Gets the value of the attribute named <paramref name="attribute"/>
+		/// </summary>
+		/// <param name="attribute"></param>
+		/// <returns></returns>
+		protected string GetAttribute(string attribute) => ((XmlField) InnerField).GetAttribute(attribute);
+	}
+}
