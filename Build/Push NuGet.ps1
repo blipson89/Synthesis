eb94ef856dbb78c4b24b566058159d2f7d07711d
Ben Lipson <blipson89@gmail.com>
2021-06-13 12:46:51 -0400

    Updated push script to not exclude Testing

--- Build/Push NuGet.ps1
+++ Build/Push NuGet.ps1
@@ -1,4 +1,4 @@
 param(
     [Parameter(Mandatory=$true)] [string] $version
 )
-gci "versions\$version\*.nupkg" -exclude *.symbols.nupkg,*.Testing* | % { ..\dependencies\NuGet.exe push $_ -Source https://api.nuget.org/v3/index.json }
+gci "versions\$version\*.nupkg" -exclude *.symbols.nupkg | % { ..\dependencies\NuGet.exe push $_ -Source https://api.nuget.org/v3/index.json }
