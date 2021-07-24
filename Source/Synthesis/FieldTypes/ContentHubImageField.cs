f4efb8b0de387a333f1ccbcba7d0a2638a651f35
Jesse Rohowetz <jesse.rohowetz@berndtgroup.net>
2021-07-23 23:59:44 -0500

    Potential additional fix for issue #93 Overriding Url to ContentHubImageField

--- Source/Synthesis/FieldTypes/ContentHubImageField.cs
+++ Source/Synthesis/FieldTypes/ContentHubImageField.cs
@@ -1,130 +1,149 @@
-﻿using System;
-using Sitecore.Data;
+﻿using Sitecore.Data;
 using Sitecore.Data.Items;
 using Synthesis.FieldTypes.Interfaces;
+using System;
 
 namespace Synthesis.FieldTypes
 {
-	public class ContentHubImageField : ImageField, IContentHubImageField
-	{
-		private string _contentId;
-		private string _thumbnailSrc;
-		private string _contentType;
+    public class ContentHubImageField : ImageField, IContentHubImageField
+    {
+        private string _contentId;
+        private string _src;
+        private string _thumbnailSrc;
+        private string _contentType;
 
-		public ContentHubImageField(LazyField field, string indexValue) : base(field, indexValue)
-		{
-		}
+        public ContentHubImageField(LazyField field, string indexValue) : base(field, indexValue)
+        {
+        }
 
-		public string ContentId
-		{
-			get
-			{
-				if (!IsContentHub)
-				{
-					return string.Empty;
-				}
+        public string ContentId
+        {
+            get
+            {
+                if (!IsContentHub)
+                {
+                    return string.Empty;
+                }
 
-				if (_contentId != null)
-				{
-					return _contentId;
-				}
+                if (_contentId != null)
+                {
+                    return _contentId;
+                }
 
-				return _contentId = GetAttribute("stylelabs-content-id");
-			}
-		}
+                return _contentId = GetAttribute("stylelabs-content-id");
+            }
+        }
 
-		public string ThumbnailSrc
-		{
-			get
-			{
-				if (!IsContentHub)
-				{
-					return string.Empty;
-				}
+        public string ThumbnailSrc
+        {
+            get
+            {
+                if (!IsContentHub)
+                {
+                    return string.Empty;
+                }
 
-				if (_thumbnailSrc != null)
-				{
-					return _thumbnailSrc;
-				}
+                if (_thumbnailSrc != null)
+                {
+                    return _thumbnailSrc;
+                }
 
-				return _thumbnailSrc = GetAttribute("thumbnailsrc");
-			}
-		}
+                return _thumbnailSrc = GetAttribute("thumbnailsrc");
+            }
+        }
 
-		public string ContentType
-		{
-			get
-			{
-				if (!IsContentHub)
-				{
-					return string.Empty;
-				}
+        public string ContentType
+        {
+            get
+            {
+                if (!IsContentHub)
+                {
+                    return string.Empty;
+                }
 
-				if (_contentType != null)
-				{
-					return _contentType;
-				}
+                if (_contentType != null)
+                {
+                    return _contentType;
+                }
 
-				return _contentType = GetAttribute("stylelabs-content-type");
-			}
-		}
+                return _contentType = GetAttribute("stylelabs-content-type");
+            }
+        }
 
-		public override MediaItem MediaItem
-		{
-			get
-			{
-				if (!IsContentHub)
-				{
-					return base.MediaItem;
-				}
+        public override string Url
+        {
+            get
+            {
+                if (!IsContentHub)
+                {
+                    return base.Url;
+                }
 
-				return null;
-			}
-		}
+                if (_src != null)
+                {
+                    return _src;
+                }
 
-		public override ID MediaItemId
-		{
-			get
-			{
-				if (!IsContentHub)
-				{
-					return base.MediaItemId;
-				}
+                return _src = GetAttribute("src");
+            }
+        }
 
-				return ID.Null;
-			}
-			set
-			{
-				if (!IsContentHub)
-				{
-					base.MediaItemId = value;
-				}
+        public override MediaItem MediaItem
+        {
+            get
+            {
+                if (!IsContentHub)
+                {
+                    return base.MediaItem;
+                }
 
-				throw new InvalidOperationException("Media Item ID cannot be set on a ContentHub image");
-			}
-		}
+                return null;
+            }
+        }
 
-		public override bool HasValue
-		{
-			get
-			{
-				if (!IsContentHub)
-				{
-					return base.HasValue;
-				}
+        public override ID MediaItemId
+        {
+            get
+            {
+                if (!IsContentHub)
+                {
+                    return base.MediaItemId;
+                }
 
-				return !string.IsNullOrEmpty(GetAttribute("src"));
-			}
-		}
+                return ID.Null;
+            }
+            set
+            {
+                if (!IsContentHub)
+                {
+                    base.MediaItemId = value;
+                }
+
+                throw new InvalidOperationException("Media Item ID cannot be set on a ContentHub image");
+            }
+        }
+
+        public override bool HasValue
+        {
+            get
+            {
+                if (!IsContentHub)
+                {
+                    return base.HasValue;
+                }
+
+                return !string.IsNullOrEmpty(GetAttribute("src"));
+            }
+        }
 
 
-		public bool IsContentHub
-		{
-			get
-			{
-				if (InnerField == null) return false;
-				return InnerField.Value?.Contains("stylelabs") ?? false;
-			}
-		}
-	}
+        public bool IsContentHub
+        {
+            get
+            {
+                if (InnerField == null) return false;
+                return InnerField.Value?.Contains("stylelabs") ?? false;
+            }
+        }
+    }
 }
