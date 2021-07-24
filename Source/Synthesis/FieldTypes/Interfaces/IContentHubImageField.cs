3b0610d1c40cac41db246b5479ece2cc03a31bdd
Jesse Rohowetz <jesse.rohowetz@berndtgroup.net>
2021-07-23 23:59:44 -0500

    Potential additional fix for issue #93 Overriding Url to ContentHubImageField

--- Source/Synthesis/FieldTypes/Interfaces/IContentHubImageField.cs
+++ Source/Synthesis/FieldTypes/Interfaces/IContentHubImageField.cs
@@ -2,19 +2,24 @@
 {
 	public interface IContentHubImageField : IImageField
 	{
-		/// <summary>
-		/// Gets the StyleLabs Content ID
-		/// </summary>
-		string ContentId { get; }
+        /// <summary>
+        /// Gets the StyleLabs Content ID
+        /// </summary>
+        string ContentId { get; }
 
-		/// <summary>
-		/// Gets the Thumbnail Src
-		/// </summary>
-		string ThumbnailSrc { get; }
+        /// <summary>
+        /// Gets the Thumbnail Src
+        /// </summary>
+        string ThumbnailSrc { get; }
 
-		/// <summary>
-		/// Gets the StyleLabs Content Type
-		/// </summary>
-		string ContentType { get; }
-	}
+        /// <summary>
+        /// Gets the StyleLabs Content Type
+        /// </summary>
+        string ContentType { get; }
+
+        /// <summary>
+        /// Returns if the image is from Content Hub
+        /// </summary>
+        bool IsContentHub { get; }
+    }
 }
