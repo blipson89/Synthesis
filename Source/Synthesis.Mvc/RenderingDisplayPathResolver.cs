aadb8bbbfbd601239b73049f1dd5de9c2857f048
Ben Lipson <blipson89@gmail.com>
2021-08-11 19:52:14 -0400

    #96 - Added setting to suppress null item error; added additional logging

--- Source/Synthesis.Mvc/RenderingDisplayPathResolver.cs
+++ Source/Synthesis.Mvc/RenderingDisplayPathResolver.cs
@@ -1,25 +1,58 @@
-﻿using Sitecore.Mvc.Presentation;
+﻿using System;
+using Sitecore.Configuration;
+using Sitecore.Diagnostics;
+using Sitecore.Mvc.Presentation;
 using Synthesis.Mvc.Pipelines.GetRenderer;
 
 namespace Synthesis.Mvc
 {
 	public class RenderingDisplayPathResolver
 	{
+		private const string SUPPRESS_NULL_ITEM_SETTING_NAME = "Synthesis.RenderingDisplayPathResolver.SuppressNullItemException";
+		private readonly bool _suppressNullItemException = Settings.GetBoolSetting(SUPPRESS_NULL_ITEM_SETTING_NAME, false);
+		/// <summary>
+		/// Gets the rendering path for the rendering
+		/// </summary>
+		/// <param name="rendering"></param>
+		/// <exception cref="NullReferenceException"></exception>
+		/// <returns></returns>
 		public virtual string ResolveRenderingPath(Rendering rendering)
 		{
 			var renderer = rendering.Renderer;
 
-			var diags = renderer as RenderingDiagnosticsInjector.DiagnosticsRenderer;
-			if (diags != null) renderer = diags.InnerRenderer;
+			if (renderer == null)
+			{
+				Log.Warn($"[{nameof(ResolveRenderingPath)}] No renderer was passed to ResolveRenderingPath", this);
+			}
 
-			var view = renderer as ViewRenderer;
-			if (view != null) return view.ViewPath;
+			if (renderer is RenderingDiagnosticsInjector.DiagnosticsRenderer diags) renderer = diags.InnerRenderer;
 
-			var controller = renderer as ControllerRenderer;
-			if (controller != null) return controller.ControllerName + "::" + controller.ActionName;
+			switch (renderer)
+			{
 
-			var method = renderer as MethodRenderer;
-			if (method != null) return method.TypeName + "." + method.MethodName + "()";
+				case ViewRenderer view:
+					return view.ViewPath;
+				case ControllerRenderer controller:
+					return controller.ControllerName + "::" + controller.ActionName;
+				case MethodRenderer method:
+					return method.TypeName + "." + method.MethodName + "()";
+			}
+
+			if (renderer != null)
+			{
+				Log.Warn($"[{nameof(ResolveRenderingPath)}] Unhandled renderer passed to {nameof(ResolveRenderingPath)}: {renderer.GetType().FullName}", this);
+			}
+
+			if (rendering.Item == null)
+			{
+				if (_suppressNullItemException)
+				{
+					Log.Error($"[{nameof(ResolveRenderingPath)}] Rendering item is null", this);
+					return string.Empty;
+				}
+
+				throw new NullReferenceException($"[{nameof(ResolveRenderingPath)}] Rendering item is null");
+			}
 
 			return rendering.Item.Paths.FullPath;
 		}
